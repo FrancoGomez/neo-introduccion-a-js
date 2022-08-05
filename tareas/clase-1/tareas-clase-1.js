479b9c92af2babf1ef9debecc5c6902ac095f64c
FrancoGomez <mgomez21699@gmail.com>
2022-08-05 13:26:38 -0300

    Creadas las funciones restantes para terminar la segunda tarea de la primer clase

--- tareas/clase-1/tareas-clase-1.js
+++ tareas/clase-1/tareas-clase-1.js
@@ -3,16 +3,61 @@
 // Preguntarle estos datos al usuario y guardarlos en 2 variables
 // Ejecutar la función con estos datos
 // Impriman el resultado en la consola
-let anioActual = prompt("Cual es el año actual?")
+/* let anioActual = prompt("Cual es el año actual?")
 let anioNacimiento = prompt("En que año naciste?")
 
 const calcularEdad = (anioActual, anioNacimiento) => {
     return anioActual - anioNacimiento
 }
 
-alert("Tenes " + calcularEdad(anioActual, anioNacimiento) + " años :)")
+alert("Tenes " + calcularEdad(anioActual, anioNacimiento) + " años :)") */
 
 
 // Preguntar el salario anual y calcular el salario mensual
 // Preguntar el salario mensual y calcular el anual
-// diario... semanal, por hora. etc.
+// diario... semanal, por hora. etc.\
+/* let salarioAnual = prompt("Cual es tu salario anual?")
+
+const calcularSalarioMensual = (salarioAnual) => {
+    const MESES_EN_ANIO = 12
+
+    return salarioAnual / MESES_EN_ANIO
+}
+
+alert("Tu salario mensual es de: $" + calcularSalarioMensual(salarioAnual)) */
+
+let salarioMensual = prompt("Cual es tu salario mensual?")
+
+const calcularSalarioAnual = (salarioMensual) => {
+    const MESES_EN_ANIO = 12
+
+    return salarioMensual * MESES_EN_ANIO
+}
+
+alert("Tu salario anual es de: $" + calcularSalarioAnual(salarioMensual))
+
+let salarioAnualUsuario = calcularSalarioAnual(salarioMensual)
+
+const calcularSalarioSemanal = (salarioAnual) => {
+    const SEMANAS_EN_ANIO = 52
+
+    return salarioAnual / SEMANAS_EN_ANIO
+}
+
+alert("Tu salario semanal es de: $" + calcularSalarioSemanal(salarioAnualUsuario))
+
+let diasTrabajadosPorAnio = prompt("Cuantas dias trabajas por año, aproximadamente?")
+
+const calcularSalarioDiario = (salarioAnual, diasTrabajadosPorAnio) => {
+    return salarioAnual / diasTrabajadosPorAnio
+}
+
+alert("Tu salario diario es de: $" + calcularSalarioDiario(salarioAnualUsuario, diasTrabajadosPorAnio))
+
+let horasTrabajadasPorDia = prompt("Cuantas horas soles trabajar por dia?")
+
+const calcularSalarioPorHora = (salarioAnual, diasTrabajadosPorAnio, horasTrabajadasPorDia) => {
+    return salarioAnual / (diasTrabajadosPorAnio * horasTrabajadasPorDia)
+}
+
+alert("Tu salario por hora es de: $" + calcularSalarioPorHora(salarioAnualUsuario, diasTrabajadosPorAnio, horasTrabajadasPorDia))
